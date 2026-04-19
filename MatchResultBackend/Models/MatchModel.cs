4ba0049d0477d7231246a39ec28821b311af2b97
feher-kristof1 <feher.kristof1@students.jedlik.eu>
2026-04-19 22:10:20 +0200

    backend

---
+++ MatchResultBackend/Models/MatchModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchResultBackend.Models
+{
+    public class MatchModel
+    {
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+    }
+}
